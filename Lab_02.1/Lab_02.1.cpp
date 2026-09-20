f69303c25a1cd00955811095163ea08e6e052f58
oleksandratelykhik2026-lab <oleksandra.telykh.ik.2026@lpnu.ua>
2026-09-20 22:58:48 +0300

    Initial commit

---
+++ Lab_02.1/Lab_02.1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    double a, z1, z2;
+
+    cout << "a= ";
+    cin >> a;
+
+    z1 = (cos(a) + sin(a)) / (cos(a) - sin(a));
+    z2 = tan(2 * a) + 1 / cos(2 * a);
+
+    cout << "z1 = " << z1 << endl;
+    cout << "z2 = " << z2 << endl;
+
+    return 0;
+}
